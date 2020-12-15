936f4fde032cebb8f747aff492a7820d0773dc3f
Crowdbotics <team@crowdbotics.com>
2020-12-15 21:02:31 +0000

    Update components

--- src/features/BlankScreen0185725/screens/blank.js
+++ src/features/BlankScreen0185725/screens/blank.js
@@ -24,13 +24,27 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { TextInput_14: "", TextInput_16: "" }
 
   render = () => (
     <View>
       <Image source={{ uri: "https://via.placeholder.com/150" }} />
       <Text>Usuario</Text>
-      <Text style={styles.Text_7}>Sample text content</Text>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_14}
+        onChangeText={nextValue => this.setState({ TextInput_14: nextValue })}
+      />
+      <Text>Contraseña</Text>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_16}
+        onChangeText={nextValue => this.setState({ TextInput_16: nextValue })}
+      />
+      <Button
+        title="Press me!"
+        onPress={() => this.props.navigation.navigate("BlankScreen1185726")}
+      />
     </View>
   )
 }
@@ -39,5 +53,8 @@ const styles = StyleSheet.create({
   View_1: {},
   Image_3: {},
   Text_5: {},
-  Text_7: { width: "100%" }
+  TextInput_14: {},
+  Text_9: {},
+  TextInput_16: {},
+  Button_12: {}
 })
